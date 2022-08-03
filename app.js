9e0a4e6259be345c234732e2f855b7d0a8890af0
Hae Lee <haeyounglee212@gmail.com>
2022-08-02 21:45:47 -0400

    'update'

--- app.js
+++ app.js
@@ -2,30 +2,43 @@ const plusImg = document.querySelector(".plusImg");
 const modal = document.querySelector(".modal");
 const inputTitle = document.querySelector(".title");
 const inputAuthor = document.querySelector(".author");
+const inputRead = document.querySelector(".read");
+const container = document.querySelector(".container");
+const containerList = document.querySelector(".containerList");
 
 const btn = document.querySelector("input[type=button]");
 
 let bookList = [];
 
-function Book(title, author) {
+function Book(title, author, read) {
   this.title = title;
   this.author = author;
-}
-function addBookToList() {
-  console.log(inputTitle.value);
-  const newBook = new Book(inputTitle.value, inputAuthor.value);
-  bookList.push(newBook);
-  modal.style.opacity = "0";
-  console.log(bookList);
-  content.createElement('div')
-  div.classList.add('list')
-  list.textContent=inputTitle.value
+  this.read = read;
 }
 
+function addBookToList() {
+  console.log(inputTitle.value);
+  const newBook = new Book(
+    inputTitle.value,
+    inputAuthor.value,
+    inputRead.value
+  );
+  bookList.push(newBook);
+}
 addBookToList();
 
 plusImg.addEventListener("click", () => {
   modal.style.opacity = "1";
 });
 
-btn.addEventListener("click", addBookToList);
+btn.addEventListener("click", () => {
+  const div = document.createElement("div");
+  div.classList.add("listItem");
+  div.innerHTML = `<p>${inputTitle.value}</p> <p>${inputAuthor.value}</p><p>${inputRead.value}</p>`;
+  containerList.appendChild(div);
+  modal.style.opacity = "0";
+  inputTitle.value = "";
+  inputAuthor.value = "";
+  inputRead.value = "";
+  console.log(bookList);
+});
